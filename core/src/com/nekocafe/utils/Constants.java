39813fe52dda0fa92a5df27b9e6fb12eeb4c1e92
IgnisDarkphoenix <167255282+IgnisDarkphoenix@users.noreply.github.com>
2025-12-15 03:13:02 -0800

    feat: add game constants

---
+++ core/src/com/nekocafe/utils/Constants.java
@@ -0,0 +1,33 @@
+package com.nekocafe.utils;
+
+public class Constants {
+    
+    // Pantalla
+    public static final int VIRTUAL_WIDTH = 720;
+    public static final int VIRTUAL_HEIGHT = 1280;
+    
+    // Tiempo de supervivencia
+    public static final float INITIAL_SURVIVAL_TIME = 30f;
+    public static final float MAX_SURVIVAL_TIME = 1800f; // 30 minutos
+    
+    // Nivel
+    public static final int MAX_LEVEL = 100;
+    
+    // Jugador
+    public static final int INITIAL_PLAYER_LIVES = 1;
+    public static final int MAX_PLAYER_LIVES = 5;
+    public static final float INVULNERABILITY_DURATION = 2.5f;
+    
+    // Gatos Chibi
+    public static final int MIN_CAT_HEALTH = 1;
+    public static final int MAX_CAT_HEALTH = 5;
+    
+    // Loot Box
+    public static final int BASE_LOOT_BOX_COST = 100;
+    public static final float LOOT_BOX_COST_INCREASE = 1.5f;
+    public static final int LOOT_BOX_INCREASE_INTERVAL = 5;
+    
+    // Modo Sin Fin
+    public static final int ENDLESS_LIVES = 3;
+    public static final float ENDLESS_DIFFICULTY_INTERVAL = 300f;
+}
